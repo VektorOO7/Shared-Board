032b21901fd0e02da0f1b39f74f0cdc013e0fcee
Viktor <h.viktor2002@gmail.com>
2024-05-21 14:17:52 +0300

    Beautifying and createBoard function

--- app/js/index.js
+++ app/js/index.js
@@ -6,13 +6,39 @@ const accountButton = document.querySelector('#account-button');
 
 const boardContainer = document.querySelector('.board-container');
 
+let boardCounter = 0;
+
+function createBoard(title, owner, description) {
+    const newBoard = document.createElement('div');
+    newBoard.classList.add('board');
+
+    const boardTitle = document.createElement('div');
+    boardTitle.classList.add('board-title');
+    boardTitle.id = 'board-title-' + boardCounter;
+    boardTitle.textContent = title;
+
+    const boardOwner = document.createElement('div');
+    boardOwner.classList.add('board-owner');
+    boardOwner.id = 'board-owner-' + boardCounter;
+    boardOwner.textContent = owner;
+
+    const boardDescription = document.createElement('div');
+    boardDescription.classList.add('board-description');
+    boardDescription.id = 'board-description-' + boardCounter;
+    boardDescription.textContent = description;
+
+    newBoard.appendChild(boardTitle);
+    newBoard.appendChild(boardOwner);
+    newBoard.appendChild(boardDescription);
+
+    boardContainer.appendChild(newBoard);
+
+    boardCounter++;
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     createBoardButton.addEventListener('click', function() {
-        const newItem = document.createElement('div');
-        
-        newItem.className = 'board';
-        
-        boardContainer.appendChild(newItem);
+        createBoard('Example Title', 'Example Owner', 'This is an example description about a board!')
     });
 
     accountButton.addEventListener('click', () => {
