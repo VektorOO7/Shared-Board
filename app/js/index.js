019dff8a90bb109c86eeab1280a391b616cb99b5
Viktor <h.viktor2002@gmail.com>
2024-05-21 11:49:49 +0300

    Did some work on the index page

--- app/js/index.js
+++ app/js/index.js
@@ -1,15 +1,22 @@
 
 
 
-document.addEventListener("DOMContentLoaded", () => {
-    const createBoardButton = document.querySelector('#create-board-button');
-    const accountButton = document.querySelector('#account-button');
+const createBoardButton = document.querySelector('#create-board-button');
+const accountButton = document.querySelector('#account-button');
 
-    createBoardButton.addEventListener('click', () => {
-        console.log("Create board button clicked");
+const boardContainer = document.querySelector('.board-container');
+
+document.addEventListener("DOMContentLoaded", () => {
+    createBoardButton.addEventListener('click', function() {
+        const newItem = document.createElement('div');
+        
+        newItem.className = 'board';
+        
+        boardContainer.appendChild(newItem);
     });
 
     accountButton.addEventListener('click', () => {
         console.log("Account button clicked");
     });
 });
+
